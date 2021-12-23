12fb0acb2c81bc2ecae86ba5a86cbf6556e0f4dd
datmon <danilanpilov@mail.ru>
2021-12-23 16:10:51 +0600

    add slider

--- script/script.js
+++ script/script.js
@@ -1,21 +1,78 @@
-const cookieWindow = document.getElementById('cookie-container');
-const acceptButton = document.getElementById('accept-button');
+const imagesArray = document.getElementById('image-array');
+const buttonLeft = document.getElementById('btn-left');
+const buttonRight = document.getElementById('btn-right');
+const images = document.getElementsByClassName('image');
+const pointers = document.getElementById('pointers');
 
-const moveDown = () => {
-  cookieWindow.style.transform = ' translateY(93px)';
-  window.localStorage.setItem('isAccepted', true);
+buttonLeft.addEventListener('click', () => movePosition(-1));
+buttonRight.addEventListener('click', () => movePosition(1));
+
+const widthOfImages = -440;
+let position = 1;
+let blockButtons = false;
+
+const numberOfImg = images.length - 2;
+
+for (let i = numberOfImg; i > 0; i--) {
+  pointers.insertAdjacentHTML(
+    'afterbegin',
+    `<div class="pointer" id="point${i}" onClick="setPosition(${i})"></div>`
+  );
 }
 
-if (window.localStorage.getItem('isAccepted')) {
-  cookieWindow.style.display = 'none';
+function movePosition(pos) {
+  if (!blockButtons) {
+    document.getElementById(`point${position}`).className = "pointer"
+    if (position + pos >= numberOfImg + 1) position = 0;
+    if (position + pos <= 0) position = numberOfImg + 1;
+    moveImage(position + pos, pos);
+    document.getElementById(`point${position + pos}`).className += " active"
+    position += pos;
+  }
 }
 
-acceptButton.addEventListener('click', moveDown);
+function setPosition(pos) {
+  if (!blockButtons) {
+    let steps = pos - position;
+    moveImage(pos, steps);
+    document.getElementById(`point${position}`).className = "pointer"
+    document.getElementById(`point${pos}`).className += " active"
+    position = pos;
+  }
+}
 
+function moveImage(currentPosition, steps) {
+  animate({
+    timing: function (timeFraction) {
+      return timeFraction;
+    },
+    draw: (progress) => {
+      imagesArray.style.transform = `translateX(${
+        // widthOfImages * currentPosition + widthOfImages * steps * progress + 'px'
+        widthOfImages * (currentPosition - steps) + widthOfImages * steps * progress + 'px'
+      })`;
+    },
+    duration: 600,
+  });
+}
 
+function animate({ timing, draw, duration }) {
+  blockButtons = true;
+  setTimeout(() => {
+    blockButtons = false;
+  }, 550);
+  let start = performance.now();
 
+  requestAnimationFrame(function animate(time) {
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
 
-/* console.log(cookieWindow.style.); */
+    let progress = timing(timeFraction);
 
+    draw(progress);
 
-
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+  });
+}
---
+++ script/slider.js
@@ -0,0 +1,83 @@
+const slider = document.getElementById('slider');
+const buttonLeft = document.getElementById('btn-left');
+const buttonRight = document.getElementById('btn-right');
+const slide = document.getElementsByClassName('slide');
+const pointers = document.getElementById('pointers');
+
+buttonLeft.addEventListener('click', () => movePosition(-1));
+buttonRight.addEventListener('click', () => movePosition(1));
+
+const widthOfslide = -440;
+let position = 1;
+let blockButtons = false;
+
+const numberOfImg = slide.length - 2;
+
+const initialization = () => {
+  for (let i = numberOfImg; i > 0; i--) {
+    pointers.insertAdjacentHTML(
+      'afterbegin',
+      `<div class="pointer" id="point${i}" onClick="setPosition(${i})"></div>`
+    );
+  }
+  document.getElementById(`point${1}`).className += ' active';
+};
+
+function movePosition(step) {
+  if (!blockButtons) {
+    document.getElementById(`point${position}`).className = 'pointer';
+    if (position + step >= numberOfImg + 1) position = 0;
+    if (position + step <= 0) position = numberOfImg + 1;
+    moveImage(position + step, step);
+    document.getElementById(`point${position + step}`).className += ' active';
+    position += step;
+  }
+}
+
+function setPosition(nextPosition) {
+  if (!blockButtons) {
+    let steps = nextPosition - position;
+    moveImage(nextPosition, steps);
+    document.getElementById(`point${position}`).className = 'pointer';
+    document.getElementById(`point${nextPosition}`).className += ' active';
+    position = nextPosition;
+  }
+}
+
+function moveImage(currentPosition, steps) {
+  animate({
+    timing: function (timeFraction) {
+      return timeFraction;
+    },
+    draw: (progress) => {
+      slider.style.transform = `translateX(${
+        // widthOfslide * currentPosition + widthOfslide * steps * progress + 'px'
+        widthOfslide * (currentPosition - steps) + widthOfslide * steps * progress + 'px'
+      })`;
+    },
+    duration: 600,
+  });
+}
+
+function animate({ timing, draw, duration }) {
+  blockButtons = true;
+  setTimeout(() => {
+    blockButtons = false;
+  }, 550);
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    let progress = timing(timeFraction);
+
+    draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+  });
+}
+
+initialization();
