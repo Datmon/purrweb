bf16360a418195acaf474d0b1da8c92595e8a6a7
Danil <danilanpilov@mail.ru>
2021-12-23 15:41:44 +0600

    init commit

---
+++ script/script.js
@@ -0,0 +1,21 @@
+const cookieWindow = document.getElementById('cookie-container');
+const acceptButton = document.getElementById('accept-button');
+
+const moveDown = () => {
+  cookieWindow.style.transform = ' translateY(93px)';
+  window.localStorage.setItem('isAccepted', true);
+}
+
+if (window.localStorage.getItem('isAccepted')) {
+  cookieWindow.style.display = 'none';
+}
+
+acceptButton.addEventListener('click', moveDown);
+
+
+
+
+/* console.log(cookieWindow.style.); */
+
+
+
